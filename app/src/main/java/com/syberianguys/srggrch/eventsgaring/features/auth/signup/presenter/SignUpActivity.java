1329f439b607ea8c2e7e994a40a1cdd28005eb46
elis.egor <E.Eliseev79@gmail.com>
2018-07-14 01:46:09 +0700

    tag loading done

--- app/src/main/java/com/syberianguys/srggrch/eventsgaring/features/auth/signup/presenter/SignUpActivity.java
+++ app/src/main/java/com/syberianguys/srggrch/eventsgaring/features/auth/signup/presenter/SignUpActivity.java
@@ -1,21 +1,30 @@
 package com.syberianguys.srggrch.eventsgaring.features.auth.signup.presenter;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.syberianguys.srggrch.eventsgaring.R;
 import com.syberianguys.srggrch.eventsgaring.features.BaseActivity;
 import com.syberianguys.srggrch.eventsgaring.features.DefaultTextWatcher;
 import com.syberianguys.srggrch.eventsgaring.features.MvpView;
+import com.syberianguys.srggrch.eventsgaring.features.event.add.presentation.AddEventActivity;
 import com.syberianguys.srggrch.eventsgaring.features.event.add.presentation.UserGender;
 import com.syberianguys.srggrch.eventsgaring.features.event.list.presentation.EventsListActivity;
+
+import java.util.Calendar;
 
 
 public final class SignUpActivity extends BaseActivity implements SignUpView {
@@ -24,9 +33,11 @@ public final class SignUpActivity extends BaseActivity implements SignUpView {
     private EditText editPassword;
     private EditText editRepeatPassword;
     private Spinner  sexList;
-    private EditText editBirthday;
+    private TextView editBirthday;
     private Button   regButton;
 
+
+    Calendar dateAndTime = Calendar.getInstance();
 
     private SignUpPresenter presenter;
     //public static boolean a=true; // чет как-то хз
@@ -75,10 +86,16 @@ public final class SignUpActivity extends BaseActivity implements SignUpView {
                 presenter.onRepeatPasswordChanged(s);
             }
         });
-        editBirthday.addTextChangedListener(new DefaultTextWatcher() {
+//        editBirthday.addTextChangedListener(new DefaultTextWatcher() {
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                presenter.onBirthdayChanged(s);
+//            }
+//        });
+        editBirthday.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                presenter.onBirthdayChanged(s);
+            public void onClick(View v) {
+               setBirthday();
             }
         });
         regButton.setOnClickListener(new View.OnClickListener() {
@@ -129,5 +146,50 @@ public final class SignUpActivity extends BaseActivity implements SignUpView {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
+    DatePickerDialog.OnDateSetListener dateSetListenerEnd = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateAndTime.clear();
+            dateAndTime.set(Calendar.YEAR, year);
+            dateAndTime.set(Calendar.MONTH, monthOfYear);
+            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setTimeEnd();
 
+        }
+    };
+
+    TimePickerDialog.OnTimeSetListener timeSetListenerEnd = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            dateAndTime.set(Calendar.MINUTE, minute);
+            setInitialBirthday();
+        }
+
+    };
+    public void setTimeEnd() {
+        new TimePickerDialog(SignUpActivity.this, timeSetListenerEnd,
+                dateAndTime.get(Calendar.HOUR_OF_DAY),
+                dateAndTime.get(Calendar.MINUTE), true)
+                .show();
+
+    }
+    private void setInitialBirthday() {
+
+
+        editBirthday.setText(DateUtils.formatDateTime(this,
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+                        | DateUtils.FORMAT_SHOW_TIME));
+        presenter.onBirthdayChanged(String.valueOf(dateAndTime.getTimeInMillis()));
+
+    }
+    public void setBirthday() {
+        new DatePickerDialog(SignUpActivity.this, dateSetListenerEnd,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH))
+                .show();
+        // setTimeEnd(v);
+    }
 }
